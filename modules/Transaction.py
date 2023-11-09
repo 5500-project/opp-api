3c230d5ef867b92f839bf38d7f77b2ad58581087
lby-rgyyt <m13051662977@gmail.com>
2023-11-08 16:24:51 -0800

    add api

--- modules/Transaction.py
+++ modules/Transaction.py
@@ -1,8 +1,39 @@
 from db.database import Base
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Float
+from sqlalchemy import (
+    Column, Integer, Float, DateTime, Enum, ForeignKey
+)
+from sqlalchemy.orm import relationship
+
+from datetime import datetime
+import enum
+
+
+class TransactionStatus(enum.Enum):
+    processed = "processed"
+    in_processing = "in_processing"
+
+
+class PaymentMethod(enum.Enum):
+    debit = "debit"
+    credit = "credit"
 
 
 class Transaction(Base):
-    __tablename__ = 'transaction'
+    __tablename__ = 'transactions'
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    amount = Column(Float, nullable=False)
+    transaction_date = Column(DateTime, default=datetime.utcnow)
+    status = Column(Enum('pending', 'completed', 'cancelled', name='transaction_status'), default='pending')
+    payment_method = Column(Enum(PaymentMethod))
+
+    #user = relationship('User', back_populates='transactions')
+
+    @classmethod
+    def get_pending_transactions(cls, session):
+        return session.query(cls).filter_by(status='pending').all()
+
+    def get_transactions_comprising_total_balance(cls, session):
+        # This method assumes that only 'completed' transactions contribute to the total balance.
+        return session.query(cls).filter_by(status='completed').all()
--- modules/Users.py
+++ modules/Users.py
@@ -1,16 +1,29 @@
 from db.database import Base
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Float
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+import bcrypt
 
 
 class Users(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True)
-    username = Column(String, unique=True)
+    id = Column(Integer, primary_key=True, index = True)
     first_name = Column(String)
-    surname = Column(String)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
-    role = Column(String)
+    last_name = Column(String)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
     phone_number = Column(String)
+    password_hash = Column(String, nullable=False)  # Renamed to indicate it's hashed
+    is_active = Column(Boolean, default= True)
+    role = Column(String)
+
+
+    # transactions = relationship('Transaction', back_populates='user')
+    # credit_cards = relationship('CreditCards', back_populates='user')
+    # debit_cards = relationship('DebitCards', back_populates='user')
+
+    def set_password(self, password):
+        self.password_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password_hash.encode('utf-8'))
